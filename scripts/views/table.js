b50a84ea11030927c84a6e8f563f231240ea43b0
timwis <tim@timwis.com>
2015-08-30 13:16:19 -0400

    Use backgrid

--- scripts/views/table.js
+++ scripts/views/table.js
@@ -1,10 +1,21 @@
 var $ = require('jquery'),
 	_ = require('underscore'),
-	Backbone = require('backbone');
-require('datatables');
-require('../../assets/js/datatables/dataTables.bootstrap');
-	
-module.exports = Backbone.View.extend({
+	Backbone = require('backbone'),
+	Backgrid = require('backgrid');
+
+var sampleColumns = [
+	{name: 'division', label: 'division', cell: 'string'},
+	{name: 'fine', label: 'fine', cell: 'string'},
+	{name: 'issue_date_and_time', label: 'issue_date_and_time', cell: 'string'},
+	{name: 'issuing_agency', label: 'issuing_agency', cell: 'string'},
+	{name: 'location', label: 'location', cell: 'string'},
+	{name: 'plate_id', label: 'plate_id', cell: 'string'},
+	{name: 'state', label: 'state', cell: 'string'},
+	{name: 'violation_description', label: 'violation_description', cell: 'string'}
+]
+
+module.exports = Backgrid.Grid.extend({
+	columns: sampleColumns,
 	initialize: function(options) {
 		options = options || {};
 		this.vent = options.vent || null;
@@ -14,11 +25,42 @@ module.exports = Backbone.View.extend({
 		
 		// Listen to vent filters
 		this.listenTo(this.vent, 'filter', this.onFilter);
+		
+		// Fetch collection
+		this.collection.fetch();
+		//this.render();
+		
+		// Call parent method
+		Backgrid.Grid.prototype.initialize.apply(this, arguments);
+	},
+});
+	
+/*module.exports = Backbone.View.extend({
+	initialize: function(options) {
+		options = options || {};
+		this.vent = options.vent || null;
+		
+		// Listen to collection
+		this.listenTo(this.collection, 'sync', this.render);
+		
+		// Listen to vent filters
+		this.listenTo(this.vent, 'filter', this.onFilter);
+		
+		// Fetch collection
+		this.collection.fetch();
+		//this.render();
 	},
 	render: function() {
+		var self = this;
+		// If table is already initialized, clear it and add the collection to it
 		if(this.table) {
 			this.$el.DataTable().clear().rows.add(this.collection.toJSON()).draw();
-		} else {
+		}
+		// Otherwise, initialize the table
+		else {
+			// Define the columns from the first model in the collection
+			// This is dangerous as the first model may not contain every property
+			// This should be done either in a config file or with a metadata request at page init
 			var columns = [];
 			if(this.collection.length) {
 				this.collection.at(0).keys().forEach(function(key) {
@@ -28,17 +70,34 @@ module.exports = Backbone.View.extend({
 						defaultContent: ''
 					});
 				});
+			} else {
+				columns = sampleColumns;
 			}
-			console.log('Creating data table', columns, this.collection.toJSON());
+			
+			// Initialize the table
+			console.log('initializing table')
 			this.table = this.$el.DataTable({
 				data: this.collection.toJSON(),
-				columns: columns
+				columns: columns,
+				scrollX: true,
+				scrollY: 400,
+				deferRender: true,
+				scroller: true,
+				ajax: function(data, callback, settings) {
+					console.log('ajax', arguments)
+					self.collection.fetch({
+						success: function(collection, response, options) {
+							console.log(collection.toJSON())
+							callback(collection.toJSON());
+						}
+					});
+				}
 			});
 		}
 	},
+	// When another chart is filtered, filter this collection
 	onFilter: function(key, value) {
-		console.log('Something else filtered', key, value);
 		this.collection.filter[key] = value;
 		this.collection.fetch();
 	}
-});
+});*/
