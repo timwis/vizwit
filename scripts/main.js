a6bfde00c3f82e3837d0e359363e6af15af0fec6
timwis <tim@timwis.com>
2015-09-04 06:24:58 -0400

    Use different config.json files for each dataset

--- scripts/main.js
+++ scripts/main.js
@@ -12,69 +12,76 @@ var DateTime = require('./views/datetime');
 var Choropleth = require('./views/choropleth');
 
 var vent = _.clone(Backbone.Events);
-var config = require('../config/parking-violations');
 
-// Render header
-if(config.header) {
-	console.log('rendering')
-	var header = new Header(config.header);
-	$('#page-header').append(header.render().el);
-}
+var dataset = window.location.hash.substr(1) || 'parking-violations';
+//var config = require('../config/parking-violations');
+$.getJSON('config/' + dataset + '.json')
+.done(function(config) {
 
-var container = $('#page-content');
-
-config.panels.forEach(function(columns) {
-	var width = Math.round(12 / columns.length);
-	var rowEl = $('<div/>').addClass('row');
+	// Render header
+	if(config.header) {
+		var header = new Header(config.header);
+		$('#page-header').append(header.render().el);
+	}
 	
-	// Loop through columns in this row
-	columns.forEach(function(column) {
-		// Add column element to row
-		var columnEl = $('<div/>').addClass('col-md-' + width);
-		rowEl.append(columnEl);
+	var container = $('#page-content');
+	
+	config.panels.forEach(function(columns) {
+		var width = Math.round(12 / columns.length);
+		var rowEl = $('<div/>').addClass('row');
 		
-		// Initialize view
-		var collection = new Socrata(null, column);
-		var filteredCollection = new Socrata(null, column);
-		
-		switch(column.chartType) {
-			case 'bar':
-				new Bar({
-					config: column,
-					el: columnEl.get(0),
-					collection: collection,
-					filteredCollection: filteredCollection,
-					vent: vent
-				});
-				break;
-			case 'datetime':
-				new DateTime({
-					config: column,
-					el: columnEl,
-					collection: collection,
-					filteredCollection: filteredCollection,
-					vent: vent
-				});
-				break;
-			case 'table':
-				new Table({
-					config: column,
-					el: columnEl,
-					collection: collection,
-					vent: vent
-				});
-				break;
-			case 'choropleth':
-				new Choropleth({
-					config: column,
-					el: columnEl,
-					collection: collection,
-					boundaries: new GeoJSON(null, column),
-					filteredCollection: filteredCollection,
-					vent: vent
-				});
-		}
+		// Loop through columns in this row
+		columns.forEach(function(column) {
+			// Add column element to row
+			var columnEl = $('<div/>').addClass('col-md-' + width);
+			rowEl.append(columnEl);
+			
+			// Initialize view
+			var collection = new Socrata(null, column);
+			var filteredCollection = new Socrata(null, column);
+			
+			switch(column.chartType) {
+				case 'bar':
+					new Bar({
+						config: column,
+						el: columnEl.get(0),
+						collection: collection,
+						filteredCollection: filteredCollection,
+						vent: vent
+					});
+					break;
+				case 'datetime':
+					new DateTime({
+						config: column,
+						el: columnEl,
+						collection: collection,
+						filteredCollection: filteredCollection,
+						vent: vent
+					});
+					break;
+				case 'table':
+					new Table({
+						config: column,
+						el: columnEl,
+						collection: collection,
+						vent: vent
+					});
+					break;
+				case 'choropleth':
+					new Choropleth({
+						config: column,
+						el: columnEl,
+						collection: collection,
+						boundaries: new GeoJSON(null, column),
+						filteredCollection: filteredCollection,
+						vent: vent
+					});
+			}
+		});
+		// Add new row w/columns to DOM
+		container.append(rowEl);
 	});
-	// Add new row w/columns to DOM
-	container.append(rowEl);
-});
+})
+.fail(function() {
+	console.error('Dataset %s not found', dataset);
+})
