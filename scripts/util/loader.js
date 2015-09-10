82e65233652c938d62aa46e0f62eeb2c888691d3
timwis <tim@timwis.com>
2015-09-10 06:48:41 -0400

    Add spin.js loading indicator. Closes #8

--- scripts/util/loader.js
+++ scripts/util/loader.js
@@ -1,7 +1,13 @@
+var Spinner = require('spin.js');
+
 exports.on = function() {
 	this.$('.card').fadeTo(1, 0.4);
+	this.spinner = new Spinner().spin(this.el);
 };
 
 exports.off = function() {
 	this.$('.card').fadeTo(1, 1);
+	if(this.spinner) {
+		this.spinner.stop();
+	}
 }
