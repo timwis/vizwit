e08a81eb6c3a6e409aeb9adcd3ccfaeb191a3896
timwis <tim@timwis.com>
2016-01-12 07:13:36 -0500

    improve deploy script, allow variable override

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+set -e && \
 cd ./dist && \
-remote_repo=`git config remote.origin.url` && \
+remote_repo=${GITHUB_TOKEN_URL:-`git config remote.origin.url`} && \
 remote_branch="gh-pages" && \
 git init && \
 git add . && \
 git commit -m'build' && \
-git push $remote_repo master:$remote_branch --force && \
+git push --force --quiet $remote_repo master:$remote_branch > /dev/null 2>&1 && \
 rm -fr .git && \
 cd ../
