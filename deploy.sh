690036ff430c5b8a357092eccee5a42ed899fc56
timwis <tim@timwis.com>
2016-01-12 07:40:34 -0500

    set git config name and email, don't run build in deploy

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e && \
-npm run build && \
 cd ./dist && \
 remote_repo=${GITHUB_URL:-`git config remote.origin.url`} && \
 remote_branch="gh-pages" && \
 git init && \
+git config user.name "Travis CI" && \
+git config user.email "nobody@nobody.org" && \
 git add . && \
 git commit -m'build' && \
 git push --force --quiet $remote_repo master:$remote_branch > /dev/null 2>&1 && \
