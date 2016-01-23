e092fdb56a11cb03e812d51eefbce944d27bc1c5
timwis <tim@timwis.com>
2016-01-23 16:24:30 -0500

    load json from sample file

--- src/scripts/vizwit-editor.js
+++ src/scripts/vizwit-editor.js
@@ -4,6 +4,8 @@ var ace = require('brace')
 ;require('brace/mode/json')
 var layout = require('./layout')
 
+var sampleData = require('../data/sample.json')
+
 var layoutOptions = {
   headerSelector: '#page-header',
   contentSelector: '#page-content'
@@ -14,6 +16,7 @@ split(['#editor', '#preview'], {sizes: [40, 60]})
 var editor = ace.edit('editor')
 var session = editor.getSession()
 session.setMode('ace/mode/json')
+editor.insert(JSON.stringify(sampleData, null, 2))
 
 var refresh = function () {
   var input = JSON.parse(editor.getValue())
