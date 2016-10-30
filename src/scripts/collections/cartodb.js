9a9e3adff89f8ac1a71e0d5bf8f5359e7cfab75a
timwis <tim@timwis.com>
2016-10-30 17:26:12 -0400

    cartodb: add leftJoin property

--- src/scripts/collections/cartodb.js
+++ src/scripts/collections/cartodb.js
@@ -15,6 +15,10 @@ module.exports = BaseProvider.extend({
     var query = squel.select()
     query.from(this.config.dataset)
 
+    if (this.config.leftJoin) {
+      query.left_join(this.config.leftJoin)
+    }
+
     // Aggregate & group by
     if (this.config.valueField || this.config.aggregateFunction || this.config.groupBy) {
       // If valueField specified, use it as the value
